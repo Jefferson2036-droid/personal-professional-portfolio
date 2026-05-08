1ee3b43ff25ce8c8d5643676fb03b461da9fd954
Jefferson Rodas <jeffersonrodas8@mac.mynetworksettings.com>
2026-05-07 20:01:49 -0400

    fix: confirmed hardcoded basePath for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  allowedDevOrigins: ["http://127.0.0.1:3000", "http://localhost:3000"],
-  images: { unoptimized: true },
+  // We are hard-coding the path to match your repo name exactly
+  basePath: "/personal-professional-portfolio",
+  assetPrefix: "/personal-professional-portfolio",
+  images: { 
+    unoptimized: true 
+  },
   trailingSlash: true,
 };
-
-if (process.env.NEXT_PUBLIC_BASE_PATH) {
-  nextConfig.basePath = process.env.NEXT_PUBLIC_BASE_PATH;
-  nextConfig.assetPrefix = process.env.NEXT_PUBLIC_BASE_PATH;
-}
 
 export default nextConfig;
